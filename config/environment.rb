ea33a1f7f8d3f5f3201263e24b4c9fb842d2e98c
richard <richard@imense.HILLS>
2012-10-10 18:52:38 +0100

    Fix so that quotes in params aren't swallowed

--- config/environment.rb
+++ config/environment.rb
@@ -129,3 +129,76 @@ if RUBY_VERSION=='1.8.7'
     end
   end
 end
+
+# Replacements for the Rack::Utils parse_query and normalize_params methods.
+# Code is from Rack 1.1.0 with the patch at 
+# http://github.com/rack/rack/commit/dae12e088592ee69545b5f2f81b87f4959859164
+# applied.
+#
+# Fixes a bug where pairs of quotes in parameter values cause the parameter
+# to be truncated. See:
+# https://rails.lighthouseapp.com/projects/8994/tickets/4808-textarea-input-silently-truncated-in-238
+#
+#  Slight modification from original patch: Before this was conditional on finding Rack 1.1.0 gem. Now,
+# it runs regardless. It seems 1.1.3 still suffers from the same problem, and on the development
+# server the gem isn't found, but the code still somehow runs.
+#
+module Rack
+  module Utils
+    def parse_query(qs, d = nil)
+      params = {}
+
+      (qs || '').split(d ? /[#{d}] */n : DEFAULT_SEP).each do |p|
+        k, v = p.split('=', 2).map { |x| unescape(x) }
+        if cur = params[k]
+          if cur.class == Array
+            params[k] << v
+          else
+            params[k] = [cur, v]
+          end
+        else
+          params[k] = v
+        end
+      end
+
+      return params
+    end
+    module_function :parse_query
+
+    def normalize_params(params, name, v = nil)
+      name =~ %r(\A[\[\]]*([^\[\]]+)\]*)
+      k = $1 || ''
+      after = $' || ''
+
+      return if k.empty?
+
+      if after == ""
+        params[k] = v
+      elsif after == "[]"
+        params[k] ||= []
+        raise TypeError, "expected Array (got #{params[k].class.name}) for param `#{k}'" unless params[k].is_a?(Array)
+        params[k] << v
+      elsif after =~ %r(^\[\]\[([^\[\]]+)\]$) || after =~ %r(^\[\](.+)$)
+        child_key = $1
+        params[k] ||= []
+        raise TypeError, "expected Array (got #{params[k].class.name}) for param `#{k}'" unless params[k].is_a?(Array)
+        if params[k].last.is_a?(Hash) && !params[k].last.key?(child_key)
+          normalize_params(params[k].last, child_key, v)
+        else
+          params[k] << normalize_params({}, child_key, v)
+        end
+      else
+        params[k] ||= {}
+        raise TypeError, "expected Hash (got #{params[k].class.name}) for param `#{k}'" unless params[k].is_a?(Hash)
+        params[k] = normalize_params(params[k], after, v)
+      end
+
+      return params
+    end
+    module_function :normalize_params
+  end
+end
+
+
+
+
