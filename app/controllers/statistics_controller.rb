e3d8f1dc0432b2f4dbfdafd4435e29fd6ef312ea
Anne Bowtell <anne.bowtell@medsci.ox.ac.uk>
2014-11-04 12:58:44 +0000

    additional statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,4 +1,6 @@
 class StatisticsController < ApplicationController
+
+before_filter :ensure_user_is_logged_in, :except => %w{ index current_series current_lists}
   
   def index
     # Users
@@ -17,5 +19,23 @@ class StatisticsController < ApplicationController
     @number_of_series = Talk.count_by_sql "select count(distinct series_id) from talks"
     @number_of_listings = @number_of_lists - @number_of_user_favourites - @number_of_venues - @number_of_series
   end
+  
+  def current_series
+    @current_series_count = Talk.count_by_sql ['select count(distinct series_id) from talks where start_time >= ?', Time.now]
+    @current_series = List.find_by_sql ['select distinct lists.* from lists, talks where (lists.id = talks.series_id) and talks.start_time >=?', Time.now]
+    @current_talks = Talk.find_public(:all, :conditions => ['start_time >= ?',Time.now])
+     
+  end
+  
+  def current_managers
+    @current_managers = Talk.find_public(:all, :conditions => ['start_time >= ?',Time.now])
+    
+  end
+  
+  def current_lists
+    @current_lists = Talk.find_public(:all, :conditions => ['start_time >= ?',Time.now])
+    
+  end
+  
   
 end
